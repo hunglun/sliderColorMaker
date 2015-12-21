84e4e72ee5e0c6f988244adcb94a04c6e006e35d
hunglun <hunglun.o@gmail.com>
2015-12-22 00:38:10 +0800

    Slider color maker v1.0

--- SliderColorMaker/ViewController.swift
+++ SliderColorMaker/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+
+    @IBOutlet var colorView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillAppear(animated: Bool) {
+        redSlider.value=0
+        greenSlider.value=0
+        blueSlider.value=0
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func changeColorInView(){
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    
+    }
 
 }
 
